a8b038232f61d92899fd58f09e240d6b241e43ec
Daniel Palacios <danielpalacios530@gmail.com>
2018-04-29 13:30:51 -0500

    worked on admin page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,5 +4,21 @@ class ApplicationsController < ApplicationController
       @guestbook_messages=Guestbook.all
    end
 
+   def create
+
+      if params[:username]==="mayraplusmario"
+         return redirect_to "/applications"
+      end
+
+      return redirect_to "/"
+
+   end
+
+   def show
+      @guestbook_messages=Guestbook.all
+      @contacts=Contact.all
+      @rsvps=User.all 
+   end
+
 
 end
