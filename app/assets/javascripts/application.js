454ffd542580c2696b14e9e26a049a2da3e4c2ce
Daniel Palacios <danielpalacios530@gmail.com>
2018-04-30 13:08:26 -0500

    fixed text structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ var x = setInterval(function() {
 
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  + minutes + "m ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
